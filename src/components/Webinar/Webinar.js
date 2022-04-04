9a50ebf15c9493de84d1450a83149398f00cf61d
Sebastian Urbaniec <55191914+surbaniec@users.noreply.github.com>
2022-04-04 12:03:54 +0200

    add desktop responsive styles to homepage (#20)

--- src/components/Webinar/Webinar.js
+++ src/components/Webinar/Webinar.js
@@ -4,12 +4,14 @@ import './Webinar.scss';
 const Webinar = () => {
   return (
     <section className='webinar'>
-      <h2 className='webinar__title'>Wypłyń na morze kariery!</h2>
-      <p className='webinar__text'>
-        Zapisz się na nasze spotkania online, które pomogą Ci wybrać kariere
-        marzeń.
-      </p>
-      <button className='webinar__btn'>Sprawdź</button>
+      <div className='webinar__wrapper'>
+        <h2 className='webinar__title'>Wypłyń na morze kariery!</h2>
+        <p className='webinar__text'>
+          Zapisz się na nasze spotkania online, które pomogą Ci wybrać kariere
+          marzeń.
+        </p>
+        <button className='webinar__btn'>Sprawdź</button>
+      </div>
     </section>
   );
 };
--- src/components/YourActivity/YourActivity.js
+++ src/components/YourActivity/YourActivity.js
@@ -7,31 +7,38 @@ import JobTile from '../JobTile/JobTile';
 const YourActivity = () => {
   return (
     <section className='your-activity'>
-      <h2 className='your-activity__title'>Twoja aktywność na Career.io</h2>
-      <div className='your-activity__controls'>
-        <button className='your-activity__btn active'>
-          <IoPhonePortraitOutline
-            style={{ height: 'auto', width: '15px', marginRight: '5px' }}
-          />
-          Ostatnio oglądane
-        </button>
-        <button className='your-activity__btn'>
-          <FiStar
-            style={{ height: 'auto', width: '15px', marginRight: '5px' }}
-          />
-          Ulubione oferty
-        </button>
-        <button className='your-activity__btn'>
-          <FiSend
-            style={{ height: 'auto', width: '15px', marginRight: '5px' }}
-          />
-          Ostatnie aplikacje
-        </button>
+      <div className='your-activity__wrapper'>
+        <h2 className='your-activity__title'>Twoja aktywność na Career.io</h2>
+        <div className='your-activity__controls'>
+          <button className='your-activity__btn active'>
+            <IoPhonePortraitOutline
+              style={{ height: 'auto', width: '15px', marginRight: '5px' }}
+            />
+            Ostatnio oglądane
+          </button>
+          <button className='your-activity__btn'>
+            <FiStar
+              style={{ height: 'auto', width: '15px', marginRight: '5px' }}
+            />
+            Ulubione oferty
+          </button>
+          <button className='your-activity__btn'>
+            <FiSend
+              style={{ height: 'auto', width: '15px', marginRight: '5px' }}
+            />
+            Ostatnie aplikacje
+          </button>
+        </div>
       </div>
+
       <div className='your-activity__tiles'>
-        <JobTile />
-        <JobTile />
-        <JobTile />
+        <div className='your-activity__wrapper'>
+          <JobTile />
+          <JobTile />
+          <JobTile />
+          <JobTile />
+          <JobTile />
+        </div>
       </div>
     </section>
   );
