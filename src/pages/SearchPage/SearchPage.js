9aba31a5698ef2bfcf9f35b556b3ae95b4782e8d
surbaniec <radsler1999@gmail.com>
2022-04-13 18:27:15 +0200

    add tablet view for search-page

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FilteredJob from '../../components/FilteredJob/FilteredJob';
 import JobFilters from '../../components/JobFilters/JobFilters';
 import JobSearch from '../../components/JobSearch/JobSearch';
 import './SearchPage.scss';
@@ -6,12 +7,28 @@ import './SearchPage.scss';
 const SearchPage = () => {
   return (
     <section className='search-page'>
-      <div className='search-page__hero'>
+      <section className='search-page__hero'>
         <JobSearch />
-      </div>
-      <div className='search-page__filters'>
+      </section>
+      <section className='search-page__filters'>
         <JobFilters />
-      </div>
+      </section>
+      <section className='search-page__recommended'>
+        <h2 className='search-page__recommended-title'>
+          Oferty pracy rekomendowane dla Ciebie
+        </h2>
+        <p className='search-page__recommended-text'>
+          Na podstawie Twojej aktywności wybraliśmy oferty dopasowane do Twoich
+          oczekiwań.
+        </p>
+        <div className='search-page__recommended-jobs'>
+          <FilteredJob />
+          <FilteredJob />
+          <FilteredJob />
+          <FilteredJob />
+          <FilteredJob />
+        </div>
+      </section>
     </section>
   );
 };
