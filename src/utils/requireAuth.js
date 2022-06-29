3747d6e0c6bb5c4d9b0e279f6e7d148e863742a2
surbaniec <radsler1999@gmail.com>
2022-06-29 15:13:55 +0200

    fix bug with protected route" "

--- src/utils/requireAuth.js
+++ src/utils/requireAuth.js
@@ -7,9 +7,9 @@ function RequireAuth() {
   const authContext = useContext(AuthContext);
   let location = useLocation();
 
-  const { isAuthenticated } = authContext;
+  const { isAuthenticated, loading } = authContext;
 
-  if (!isAuthenticated) {
+  if (!isAuthenticated && !loading) {
     // Redirect them to the /login page, but save the current location they were
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
