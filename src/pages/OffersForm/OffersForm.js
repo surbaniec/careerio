1cdfcbb993cba259dc31b09a3c4b0c602cefb5fd
surbaniec <radsler1999@gmail.com>
2022-07-05 11:28:23 +0200

    refactor html css code for Offers Form page

--- src/pages/OffersForm/OffersForm.js
+++ src/pages/OffersForm/OffersForm.js
@@ -2,6 +2,10 @@ import React, { useContext, useEffect, useState } from 'react';
 import './OffersForm.scss';
 import AuthContext from '../../context/auth/authContext';
 import Select from 'react-select';
+import { Link } from 'react-router-dom';
+import { MdOutlineSpaceDashboard, MdListAlt, MdAdd } from 'react-icons/md';
+import { CgProfile } from 'react-icons/cg';
+import { IoReceiptOutline } from 'react-icons/io5';
 
 const employmentTypeOptions = [
   { value: 'b2b', label: 'B2B' },
@@ -43,6 +47,7 @@ const selectStyles = {
     border: '1px solid #f3f3f3',
     borderRadius: '10px',
     padding: '5px',
+    marginTop: '5px',
     marginRight: '5px',
     fontSize: '1rem',
     lineHeight: '1.75rem',
@@ -81,134 +86,224 @@ const OffersForm = () => {
 
   return (
     <>
-      <section className='top'></section>
-      <div className='offersform-page-wrapper'>
-        <div className='left-wrap'>
-          <div className='icons'>
-            <p>Profil pracodawcy</p>
-          </div>
-          <div className='icons'>
-            <p>Panel pracodawcy</p>
-          </div>
-          <div className='icons'>
-            <p>Artykuły</p>
-          </div>
-          <div className='icons'>
-            <p>Oferty pracy</p>
-          </div>
-          <div className='icons'>
-            <p>Faktury</p>
-          </div>
-        </div>
-        <div className='right-wrap'>
-          <div className='big-box2'>
-            <div className='offer-box2'>
-              <div className='login-title'>
-                <h2>Dodaj lub edytuj ogłoszenie</h2>
-              </div>
-              <form className='form-wrapper'>
-                <p>Nazwa stanowiska</p>
-                <input
-                  className='login-name'
-                  type='text'
-                  placeholder='Wpisz nazwę stanowiska...'
-                />
-                <p>Poziom doświadczenia</p>
-                <Select
-                  options={experienceLevelOptions}
-                  styles={selectStyles}
-                  components={{
-                    IndicatorSeparator: () => null,
-                  }}
-                  defaultValue={{
-                    label: 'Wybierz poziom doświadczenia',
-                    value: null,
-                  }}
-                  // onChange={(e) =>
-                  //   setEmploymentOption({ value: e.value, label: e.label })
-                  // }
-                />
-                <p>Rodzaj umowy</p>
-                <Select
-                  options={employmentTypeOptions}
-                  styles={selectStyles}
-                  components={{
-                    IndicatorSeparator: () => null,
-                  }}
-                  defaultValue={{ label: 'Wybierz rodzaj umowy', value: null }}
-                  // onChange={(e) =>
-                  //   setEmploymentOption({ value: e.value, label: e.label })
-                  // }
-                />
-                <p>Wymiar czasu pracy</p>
-                <Select
-                  options={workingHoursOptions}
-                  styles={selectStyles}
-                  components={{
-                    IndicatorSeparator: () => null,
-                  }}
-                  defaultValue={{
-                    label: 'Wybierz wymiar czasu pracy',
-                    value: null,
-                  }}
-                  // onChange={(e) =>
-                  //   setEmploymentOption({ value: e.value, label: e.label })
-                  // }
-                />
-                <p>Widełki płacowe</p>
-                <label htmlFor='salaryFrom'>Od: </label>
-                <input
-                  type='number'
-                  name='salaryFrom'
-                  id='salaryFrom'
-                  min={0}
-                  max={50000}
-                  required
-                />
+      <section className='offers-form'>
+        <div className='offers-form__page-wrapper'>
+          <div className='offers-form__left-wrap'>
+            <ul className='dashboard-menu'>
+              <li className='dashboard-menu__item'>
+                <Link to='' className='dashboard-menu__link'>
+                  <div className='dashboard-menu__tile'>
+                    <MdOutlineSpaceDashboard
+                      style={{ width: '20px', height: 'auto', color: '#fff' }}
+                    />
+                    <span>Panel pracodawcy</span>
+                  </div>
+                </Link>
+              </li>
+              <li className='dashboard-menu__item'>
+                <Link to='' className='dashboard-menu__link '>
+                  <div className='dashboard-menu__tile '>
+                    <CgProfile
+                      style={{ width: '20px', height: 'auto', color: '#fff' }}
+                    />
+                    <span>Profil pracodawcy</span>
+                  </div>
+                </Link>
+              </li>
+              <li className='dashboard-menu__item'>
+                <Link to='' className='dashboard-menu__link'>
+                  <div className='dashboard-menu__tile'>
+                    <MdListAlt
+                      style={{ width: '20px', height: 'auto', color: '#fff' }}
+                    />
 
-                <label htmlFor='salaryTo'>Do: </label>
-                <input
-                  type='number'
-                  name='salaryTo'
-                  id='salaryTo'
-                  min={0}
-                  max={50000}
-                />
-
-                <p>Zakres obowiązków</p>
-                <textarea
-                  className='login-name'
-                  type='textarea'
-                  placeholder='Wpisz zakres obowiązków, rozdzielając średnikami...'
-                />
-                <p>Wymagania wobec kandydata</p>
-                <textarea
-                  className='login-name'
-                  type='textarea'
-                  placeholder='Wpisz wymagania wobec kandydata, rozdzielając średnikami...'
-                />
-
-                <label
-                  className='offers-form__label'
-                  htmlFor='remoteRecruitment'
+                    <span>Oferty pracy</span>
+                  </div>
+                </Link>
+              </li>
+              <li className='dashboard-menu__item'>
+                <Link
+                  to=''
+                  className='dashboard-menu__link dashboard-menu__link--active'
                 >
-                  Rekrutacja zdalna
-                </label>
-                <input
-                  className='offers-form__checkbox'
-                  type='checkbox'
-                  name='remoteRecruitment'
-                  id='remoteRecruitment'
-                />
+                  <div className='dashboard-menu__tile dashboard-menu__tile--active'>
+                    <MdAdd
+                      style={{
+                        width: '20px',
+                        height: 'auto',
+                        color: '#575757',
+                      }}
+                    />
+                    <span>Dodaj ofertę</span>
+                  </div>
+                </Link>
+              </li>
+              <li className='dashboard-menu__item'>
+                <Link to='' className='dashboard-menu__link'>
+                  <div className='dashboard-menu__tile'>
+                    <IoReceiptOutline
+                      style={{ width: '20px', height: 'auto', color: '#fff' }}
+                    />
+                    <span>Faktury</span>
+                  </div>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className='offers-form__right-wrap'>
+            <div className='offers-form__box-wrapper'>
+              <div className='offers-form__box'>
+                <div className='offers-form__title-wrapper'>
+                  <h2 className='offers-form__title'>
+                    Dodaj lub edytuj ogłoszenie
+                  </h2>
+                </div>
+                <form className='offers-form__form'>
+                  <label htmlFor='jobPosition' className='offers-form__label'>
+                    Nazwa stanowiska
+                  </label>
+                  <input
+                    className='offers-form__input'
+                    type='text'
+                    name='jobPosition'
+                    placeholder='Wpisz nazwę stanowiska...'
+                  />
+                  <label
+                    htmlFor='experienceLevel'
+                    className='offers-form__label'
+                  >
+                    Poziom doświadczenia
+                  </label>
+                  <Select
+                    name='experienceLevel'
+                    options={experienceLevelOptions}
+                    styles={selectStyles}
+                    components={{
+                      IndicatorSeparator: () => null,
+                    }}
+                    defaultValue={{
+                      label: 'Wybierz poziom doświadczenia',
+                      value: null,
+                    }}
+                    // onChange={(e) =>
+                    //   setEmploymentOption({ value: e.value, label: e.label })
+                    // }
+                  />
+                  <label
+                    htmlFor='employmentType'
+                    className='offers-form__label'
+                  >
+                    Rodzaj umowy
+                  </label>
+                  <Select
+                    name='employmentType'
+                    options={employmentTypeOptions}
+                    styles={selectStyles}
+                    components={{
+                      IndicatorSeparator: () => null,
+                    }}
+                    defaultValue={{
+                      label: 'Wybierz rodzaj umowy',
+                      value: null,
+                    }}
+                    // onChange={(e) =>
+                    //   setEmploymentOption({ value: e.value, label: e.label })
+                    // }
+                  />
+                  <label htmlFor='workingHours' className='offers-form__label'>
+                    Wymiar czasu pracy
+                  </label>
+                  <Select
+                    name='workingHours'
+                    options={workingHoursOptions}
+                    styles={selectStyles}
+                    components={{
+                      IndicatorSeparator: () => null,
+                    }}
+                    defaultValue={{
+                      label: 'Wybierz wymiar czasu pracy',
+                      value: null,
+                    }}
+                    // onChange={(e) =>
+                    //   setEmploymentOption({ value: e.value, label: e.label })
+                    // }
+                  />
+                  <p className='offers-form__p'>Widełki płacowe</p>
+                  <label
+                    htmlFor='salaryFrom'
+                    className='offers-form__label inline'
+                  >
+                    Od:{' '}
+                  </label>
+                  <input
+                    className='offers-form__input'
+                    type='number'
+                    name='salaryFrom'
+                    id='salaryFrom'
+                    min={0}
+                    max={50000}
+                    required
+                  />
 
-                <button className='login-submit' type='submit'>
-                  Zapisz zmiany
-                </button>
-              </form>
+                  <label
+                    htmlFor='salaryTo'
+                    className='offers-form__label inline'
+                  >
+                    Do:{' '}
+                  </label>
+                  <input
+                    className='offers-form__input'
+                    type='number'
+                    name='salaryTo'
+                    id='salaryTo'
+                    min={0}
+                    max={50000}
+                  />
+
+                  <label htmlFor='jobDuties' className='offers-form__label'>
+                    Zakres obowiązków
+                  </label>
+                  <textarea
+                    className='offers-form__textarea'
+                    type='textarea'
+                    name='jobDuties'
+                    placeholder='Wpisz zakres obowiązków, rozdzielając średnikami...'
+                  />
+                  <label
+                    htmlFor='jobRequirements'
+                    className='offers-form__label'
+                  >
+                    Wymagania wobec kandydata
+                  </label>
+                  <textarea
+                    className='offers-form__textarea'
+                    type='textarea'
+                    name='jobRequirements'
+                    placeholder='Wpisz wymagania wobec kandydata, rozdzielając średnikami...'
+                  />
+
+                  <label
+                    className='offers-form__label inline'
+                    htmlFor='remoteRecruitment'
+                  >
+                    Rekrutacja zdalna
+                  </label>
+                  <input
+                    className='offers-form__checkbox'
+                    type='checkbox'
+                    name='remoteRecruitment'
+                    id='remoteRecruitment'
+                  />
+
+                  <button className='offers-form__btn' type='submit'>
+                    Zapisz zmiany
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
